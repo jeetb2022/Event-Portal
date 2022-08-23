57d8e79dd4b9e94f39ddbaf3bfe5f746dab6cd2b
Jeet <jeetbhadaniya1228@gmail.com>
2022-08-23 22:26:30 +0530

    Post req of form added

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ const port = 80;
 
 app.use("/static",express.static("static"));
 
+app.use(express.urlencoded());
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+'/static/index.html');
 })
 app.get("/form",(req,res)=>{
-    res.sendFile(__dirname+'/form.html');
+    res.sendFile(__dirname+'/static/form.html');
+})
+
+app.post("/",(req,res)=>{
+    let Name = req.body;
+    console.log(req.body.name);
+    console.log(req.body.email);
+    console.log(req.body.roll);
 })
 
 app.listen(port,()=>{
